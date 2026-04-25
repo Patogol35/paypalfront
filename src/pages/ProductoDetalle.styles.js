45f144ca70e25c1b521a6162232060d364f8646d
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-24 19:54:12 -0500

    Update ProductoDetalle.styles.js

--- src/pages/ProductoDetalle.styles.js
+++ src/pages/ProductoDetalle.styles.js
@@ -13,17 +13,21 @@ export const containerSx = {
   gap: 4,
 };
 
-export const dividerSx = (theme) => ({
-  width: "100%",
-  my: 2,
-  height: "1px",
-  border: "none",
-
-  background:
+export const dividerSx = (theme) => {
+  const borderColor =
     theme.palette.mode === "dark"
-      ? "linear-gradient(to right, transparent, rgba(255,255,255,0.18), transparent)"
-      : "linear-gradient(to right, transparent, rgba(0,0,0,0.2), transparent)",
-});
+      ? "rgba(255,255,255,0.35)"
+      : "rgba(0,0,0,0.25)";
+
+  return {
+    width: "100%",
+    my: 2,
+    height: "1px",
+    border: "none",
+
+    backgroundColor: borderColor,
+  };
+};
 
 // ================================
 // BOTÓN VOLVER
